d046e255cdb3ded6fee035d3f995fee8eb0a24ba
Danilo Ramos <130659168+RedPartyDan@users.noreply.github.com>
2024-06-01 12:57:48 -0300

    Envio de dados para Templates (Sending data to Templates)

--- 05-django/main/views.py
+++ 05-django/main/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    context = {
+        'nome': "Fiallraven - Foldsack No.1 Backpack, Fits 15 Laptops",
+        'preço': 109.95,
+        'descricao': "Your perfect pack for everyday is and walks in the forest. Stash your Laptop (up to 15 inches) in the padden sleeve, your everyday",
+        'categoria': "Men's Clothing",
+        'imagem': "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+    }
     return render(request, 'main/index.html')
 
 
