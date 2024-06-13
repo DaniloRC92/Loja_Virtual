769494c65b5adb92000fbe239ee615dfc60eaf40
Danilo Ramos <130659168+RedPartyDan@users.noreply.github.com>
2024-06-13 20:17:21 -0300

    Adjusment load static

--- 05-django/lojavirtual/settings.py
+++ 05-django/lojavirtual/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t!k=s89br^74h%5417b6!7%@rrt^76ymifm57x5(4x=36di*64'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 # para carregar arquivos estáticos em ambiente de desenvolvimento
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 # o comando a seguir faz a mesma coisa que o acima, ou seja
